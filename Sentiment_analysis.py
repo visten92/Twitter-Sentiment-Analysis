5909e2e649642c2f47c65681b612d40328f1fdea
Stefanos Nikolopoulos <stefnikolopoulos@gmail.com>
2023-01-20 13:40:24 +0200

    Update Sentiment_analysis.py

--- Sentiment_analysis.py
+++ Sentiment_analysis.py
@@ -2,22 +2,27 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from scipy.special import softmax
 import tweepy
 
+# Credentials
 API_KEY = 'YOUR_API_KEY'
 API_SECRET_KEY = 'YOUR_SECRET_KEY'
 
+# Establish Connection
 auth = tweepy.OAuth1UserHandler(
   API_KEY ,
   API_SECRET_KEY
 )
 
+# Search tweets by keyword until the desired date
+keyword = "Stock market"
 date = '2023-01-17'
 api = tweepy.API(auth)
-tweets = api.search_tweets("Stock market", tweet_mode="extended", until=date)
+tweets = api.search_tweets(q = keyword, tweet_mode="extended", until=date)
 
-
+# Load RoBERTa model
 tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
 model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
 
+# Use the model on each tweet to perform sentiment analysis and possitivity score (score > 0 : possitive, score < 0 : negative)
 score_coef = [-1,0,1]
 labels = ['Negative', 'Neutral', 'Positive']
 total_score = 0
